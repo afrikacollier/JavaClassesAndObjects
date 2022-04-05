d6393eab56b0871abfd2f1c5a28bca8794599827
afrikacollier <101090240+afrikacollier@users.noreply.github.com>
2022-04-04 23:05:25 -0700

    Update Pet.java

--- JavaClassesandObjects/src/Pet.java
+++ JavaClassesandObjects/src/Pet.java
@@ -1,4 +1,36 @@
 
 public class Pet {
+	private String name;
+	private int age;
+	private String location;
+	private String type;
 
+
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+	
 }
